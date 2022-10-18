c5b06b9a68deb8b346d4313fe5b21a7f77607094
Scott Sun <59678188+scottsun2020@users.noreply.github.com>
2022-10-18 12:02:25 -0700

    Add Health Bar System

--- MY3DGAME/Assets/Scripts/PlayerStats.cs
+++ MY3DGAME/Assets/Scripts/PlayerStats.cs
@@ -4,5 +4,36 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    public float health = 100f;
+    public float maxHealth = 100f;
+    public float currentHealth;
+
+    public HealthBar healthBar;
+
+    void Start(){
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.K)){
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(float damage){
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+    }
+
+    public void HealthBoost(float boost){
+        currentHealth += boost;
+
+        if(currentHealth > maxHealth){
+            currentHealth = maxHealth;
+        }
+
+        healthBar.SetHealth(currentHealth);
+    }
+
 }
